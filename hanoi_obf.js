6a549e6ed4877350a0bebf2cbbda320948bff190
Tanoy Sinha <tsinha@gmail.com>
2014-11-18 15:21:30 -0500

    obf part 1

--- hanoi_obf.js
+++ hanoi_obf.js
@@ -1,25 +1,17 @@
-function ga(n) {
-  var a = [];
-  for(var i=n-1; i>0; i--) { a.push(i); }
-  return a;
-}
-
+function ga(n) { return Array.apply(null, Array(n)).map(function(i, ind) { return ind; }).reverse(); }
 var cn = document.getElementById('cn'),
     cx = cn.getContext('2d'),
     W = 20,
     MW = W * 10,
     SZ = window.innerWidth - MW;
-
 cn.width = window.innerWidth;
 cn.height = window.innerHeight;
-
 var H = function(t) {
   this.A = ga(t);
   this.B = [];
   this.C = [];
   this.i = 0;
 };
-
 H.prototype = {
   hn: function(n, a, b, c) {
     if(n>0) {
@@ -28,18 +20,15 @@ H.prototype = {
       this.hn(n-1,b,a,c);
     }
   },
-
   gq: function() {
     this.q=[];
     this.hn(this.A.length, this.A, this.B, this.C);
   },
-
   r: function() {
     this.s();
     this.rn();
-    window.setTimeout(this.r.bind(this), 1000);
+    window.setTimeout(this.r.bind(this), 500);
   },
-
   s: function() {
     if(this.i > this.q.length) return;
     var it = this.q[this.i],
@@ -47,14 +36,12 @@ H.prototype = {
     it[1].push(vl);
     this.i++;
   },
-
   rn: function() {
     cn.width = cn.width;
     this.rt(MW, this.A);
     this.rt(SZ / 2, this.B);
     this.rt(SZ - MW, this.C);
   },
-
   rt: function(x, a) {
     for(var i=0; i < a.length; i++) {
       var w = a[i] * W;
@@ -62,8 +49,6 @@ H.prototype = {
     }
   }
 };
-
 var hs = new H(7);
-
 hs.gq();
 hs.r();
