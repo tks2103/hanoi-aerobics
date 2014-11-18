2d540b07c60ad89496df5ec64b62e2b0450e1d3c
Tanoy Sinha <tsinha@gmail.com>
2014-11-18 16:25:50 -0500

    more linesss

--- hanoi_obf.js
+++ hanoi_obf.js
@@ -1,15 +1,11 @@
 cn = document.body.children[0],
-cx = cn.getContext('2d'),
-W = 20,
-MW = W * 10,
-SZ = window.innerWidth - MW;
+cx = cn.getContext('2d');
 function ga(n) { return Array.apply(null, Array(n)).map(function(i, ind) { return ind; }).reverse(); }
-cn.width = window.innerWidth;
-cn.height = window.innerHeight;
-A = ga(5);
-B = [];
-C = [];
-it = 0;
+cn.width = 1000;
+cn.height = 600;
+x = ga(7);
+y = [];
+z = [];
 i = 0;
 function hn(n, a, b, c) {
   if(n>0) {
@@ -20,28 +16,22 @@ function hn(n, a, b, c) {
 }
 function gq() {
   q = [];
-  hn(A.length, A, B, C);
+  hn(x.length, x, y, z);
 }
 function r() {
-  s();
-  rn();
+  if(i > q.length) return;
+  it = q[i], vl = it[0].pop();
+  it[1].push(vl);
+  i++;
+  cn.width = cn.width;
+  rt(260, x);
+  rt(500, y);
+  rt(740, z);
   window.setTimeout(r, 500);
 }
-function s() {
-  if(i > q.length) return;
-  itt = q[i], vl = itt[0].pop();
-  itt[1].push(vl);
-  i++;
-}
-function rn() {
-  cn.width = cn.width;
-  rt(MW, A);
-  rt(SZ / 2, B);
-  rt(SZ - MW, C);
-}
 function rt(x, a) {
-  a.map(function(w,i) {
-    cx.fillRect(x - w * W / 2, window.innerHeight / 2 - i * W, w*W, W);
+  a.map(function(c,i) {
+    cx.fillRect(x - c * 5, window.innerHeight / 2 - i * 10, c*10, 10);
   });
 }
 gq();
