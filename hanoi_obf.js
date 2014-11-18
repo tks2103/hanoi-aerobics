4998bb586fdf82649073632d0d18c1934d8377e5
Tanoy Sinha <tsinha@gmail.com>
2014-11-18 15:50:07 -0500

    freal

--- hanoi_obf.js
+++ hanoi_obf.js
@@ -1,54 +1,48 @@
+cn = document.body.children[0],
+cx = cn.getContext('2d'),
+W = 20,
+MW = W * 10,
+SZ = window.innerWidth - MW;
 function ga(n) { return Array.apply(null, Array(n)).map(function(i, ind) { return ind; }).reverse(); }
-var cn = document.getElementById('cn'),
-    cx = cn.getContext('2d'),
-    W = 20,
-    MW = W * 10,
-    SZ = window.innerWidth - MW;
 cn.width = window.innerWidth;
 cn.height = window.innerHeight;
-var H = function(t) {
-  this.A = ga(t);
-  this.B = [];
-  this.C = [];
-  this.i = 0;
-};
-H.prototype = {
-  hn: function(n, a, b, c) {
-    if(n>0) {
-      this.hn(n-1,a,c,b);
-      this.q.push([a,c]);
-      this.hn(n-1,b,a,c);
-    }
-  },
-  gq: function() {
-    this.q=[];
-    this.hn(this.A.length, this.A, this.B, this.C);
-  },
-  r: function() {
-    this.s();
-    this.rn();
-    window.setTimeout(this.r.bind(this), 500);
-  },
-  s: function() {
-    if(this.i > this.q.length) return;
-    var it = this.q[this.i],
-        vl = it[0].pop();
-    it[1].push(vl);
-    this.i++;
-  },
-  rn: function() {
-    cn.width = cn.width;
-    this.rt(MW, this.A);
-    this.rt(SZ / 2, this.B);
-    this.rt(SZ - MW, this.C);
-  },
-  rt: function(x, a) {
-    for(var i=0; i < a.length; i++) {
-      var w = a[i] * W;
-      cx.fillRect(x - w / 2, window.innerHeight / 2 - i * W, w, W);
-    }
+A = ga(5);
+B = [];
+C = [];
+it = 0;
+i = 0;
+function hn(n, a, b, c) {
+  if(n>0) {
+    hn(n-1,a,c,b);
+    q.push([a,c]);
+    hn(n-1,b,a,c);
   }
-};
-var hs = new H(7);
-hs.gq();
-hs.r();
+}
+function gq() {
+  q = [];
+  hn(A.length, A, B, C);
+}
+function r() {
+  s();
+  rn();
+  window.setTimeout(r, 500);
+}
+function s() {
+  if(i > q.length) return;
+  itt = q[i], vl = itt[0].pop();
+  itt[1].push(vl);
+  i++;
+}
+function rn() {
+  cn.width = cn.width;
+  rt(MW, A);
+  rt(SZ / 2, B);
+  rt(SZ - MW, C);
+}
+function rt(x, a) {
+  a.map(function(w,i) {
+    cx.fillRect(x - w * W / 2, window.innerHeight / 2 - i * W, w*W, W);
+  });
+}
+gq();
+r();
